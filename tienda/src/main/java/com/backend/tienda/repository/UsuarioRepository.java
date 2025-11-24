0b7b3a4fac47c1980fbf25cb21529dcedca9e863
Mayckol2005 <may.mardones@duocuc.cl>
2025-11-24 20:58:41 -0300

    Act. user

--- tienda/src/main/java/com/backend/tienda/repository/UsuarioRepository.java
+++ tienda/src/main/java/com/backend/tienda/repository/UsuarioRepository.java
@@ -5,10 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    // Método mágico: Spring entiende que buscas por 'correo'
     Optional<Usuario> findByCorreo(String correo);
 
-    // Para validar que no se repita el RUT o Correo al registrarse
     boolean existsByCorreo(String correo);
     boolean existsByRut(String rut);
 }
