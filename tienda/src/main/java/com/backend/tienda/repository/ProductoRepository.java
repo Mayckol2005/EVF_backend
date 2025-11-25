b912b05e5b2ab12f3c199b6ea912906c185044a8
Mayckol2005 <may.mardones@duocuc.cl>
2025-11-24 21:00:37 -0300

    Cambio product

--- tienda/src/main/java/com/backend/tienda/repository/ProductoRepository.java
+++ tienda/src/main/java/com/backend/tienda/repository/ProductoRepository.java
@@ -7,15 +7,11 @@ import java.util.List;
 
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
 
-    // Buscar por categoría (ej: perfumes-varon)
     List<Producto> findByCategoriaId(String categoriaId);
 
-    // Buscar ofertas (donde precio < precio normal)
-    // Aquí usamos JPQL (SQL de Java)
     @Query("SELECT p FROM Producto p WHERE p.price < p.normalPrice")
     List<Producto> findOfertas();
 
-    // Buscar productos con poco stock (para el Dashboard Admin)
     @Query("SELECT p FROM Producto p WHERE p.stock < 10")
     List<Producto> findLowStock();
 }
