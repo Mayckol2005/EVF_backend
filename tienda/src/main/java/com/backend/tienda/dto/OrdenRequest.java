e0107fda61b97100a52735d4225d91ea5c6e89fe
Mayckol2005 <may.mardones@duocuc.cl>
2025-11-24 21:02:08 -0300

    Cambios order

--- tienda/src/main/java/com/backend/tienda/dto/OrdenRequest.java
+++ tienda/src/main/java/com/backend/tienda/dto/OrdenRequest.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 @Data
 public class OrdenRequest {
-    // Solo necesitamos esto del frontend.
-    // El resto (precios, totales, usuario) lo calculamos en el backend por seguridad.
+
     private List<ItemRequest> items;
 }
